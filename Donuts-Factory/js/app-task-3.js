0f196a87ec2931b7053a2f4f101ff8f83e49355a
Muhammad_Shurrab <bsmuhammad68@gmail.com>
2024-06-06 00:36:04 +0300

    Solve quiz 4

--- Donuts-Factory/js/app-task-3.js
+++ Donuts-Factory/js/app-task-3.js
@@ -27,10 +27,36 @@
 // Expected Output : 'aegnor'
 // Assume punctuation and numbers symbols are not included in the passed string.
 
-// let gojo = prompt("Please enter a number");
-// let reversed = gojo.split("").sort().join("");
-// alert(reversed);
+let gojo = prompt("Please enter a number");
+let reversed = gojo.split("").sort().join("");
+alert(reversed);
 
+// Q4 :
+//  Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.
+
+// Prompt the user for input and store it in the 'num' variable
+var num = window.prompt();
+
+// Convert the number to a string and store it in the 'str' variable
+var str = num.toString();
+
+// Initialize an array 'result' with the first character of the string
+var result = [str[0]];
+
+// Iterate through the characters of the string starting from index 1
+for (var x = 1; x < str.length; x++) {
+  // Check if the previous and current characters are both even digits
+  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+    // If both are even, push a '-' and the current character to the 'result' array
+    result.push("-", str[x]);
+  } else {
+    // If not both even, push the current character to the 'result' array
+    result.push(str[x]);
+  }
+}
+
+// Output the joined 'result' array as a string
+console.log(result.join(""));
 // Q5:
 // Write a function (Agechecker) that checks age of the user if the age is larger than or equal 18, the output will be ( The user is Adult ) else ( The user is Minor)
 // Use ternary operator
